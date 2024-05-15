f295df6c2babc01ed126440ff286d36130aa9037
Hardik <neopanehardik630@gmail.com>
2024-05-15 23:59:03 +0545

    Firebase

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  push,
-} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
-const appSettings = {
+const firebaseConfig = {
+  apiKey: "AIzaSyCoS0ipK_rOm0ZSeB5d_ehhVBJTq6o2_RE",
+  authDomain: "playground-9d131.firebaseapp.com",
   databaseURL:
-    "https://playground-9d131-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    "https://playground-9d131-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "playground-9d131",
+  storageBucket: "playground-9d131.appspot.com",
+  messagingSenderId: "425804815503",
+  appId: "1:425804815503:web:43dd78a6b82672d0eae4fa",
 };
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const moviesInDB = ref(database, "movies");
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const notesInDB = ref(database, "notes");
-push(notesInDB, "Hello");
+const app = initializeApp(firebaseConfig);
