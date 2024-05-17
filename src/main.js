0488194a964c9ffc6be3f013dd5388c0d2f0dbe8
Hardik <neopanehardik630@gmail.com>
2024-05-17 21:36:40 +0545

    firebase

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCoS0ipK_rOm0ZSeB5d_ehhVBJTq6o2_RE",
+  authDomain: "playground-9d131.firebaseapp.com",
+  databaseURL:
+    "https://playground-9d131-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "playground-9d131",
+  storageBucket: "playground-9d131.appspot.com",
+  messagingSenderId: "425804815503",
+  appId: "1:425804815503:web:43dd78a6b82672d0eae4fa",
+};
+
+const app = initializeApp(firebaseConfig);
+const databse = getDatabase(app);
+const NotesInDB = ref(databse, "Notes");
+
+set(NotesInDB, "Hello");
